5cf94f78b20c7b0d8310f61b5fd15b81ed793d96
Gancarz, Szymon S.J. <s.gancarz@student.fontys.nl>
2024-09-17 10:45:42 +0200

    Seeing if it works

---
+++ Vinylo-Individual/src/main/java/s3/individual/vinylo/VinyloIndividualApplication.java
@@ -0,0 +1,13 @@
+package s3.individual.vinylo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinyloIndividualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinyloIndividualApplication.class, args);
+    }
+
+}
---
+++ Vinylo-Individual/src/test/java/s3/individual/vinylo/VinyloIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package s3.individual.vinylo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinyloIndividualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
