7e94f6951ea2bb27438f025d5dc4aca5b2228ae6
I540927 <s.gancarz@student.fontys.nl>
2024-10-29 11:20:12 +0100

    Adujsted the build gradle to use sonarqube

--- Vinylo-Individual/src/main/java/s3/individual/vinylo/configuration/WebConfig.java
+++ Vinylo-Individual/src/main/java/s3/individual/vinylo/configuration/WebConfig.java
@@ -2,8 +2,10 @@ package s3.individual.vinylo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,11 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                //This allows CORS for all paths
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        //This allows all headers to request.
                         .allowedHeaders("*")
+                        //This allows credentials like for example cookies
                         .allowCredentials(true);
             }
         };
