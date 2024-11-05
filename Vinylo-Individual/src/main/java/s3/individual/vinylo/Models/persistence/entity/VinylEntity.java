2f5ac866ecd0236edc4ab7afc940d1f41c686069
I540927 <s.gancarz@student.fontys.nl>
2024-11-05 11:00:38 +0100

    Tables are now loading in the database. I need to work on displaying data in them.

---
+++ Vinylo-Individual/src/main/java/s3/individual/vinylo/Models/persistence/entity/VinylEntity.java
@@ -0,0 +1,47 @@
+package s3.individual.vinylo.Models.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "vinyl")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VinylEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "vinylType")
+    private String vinylType;
+
+    @NotNull
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @Length(max = 150)
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "isReleased")
+    private Boolean isReleased;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "artist_id")
+    private ArtistEntity artist;
+
+}
---
+++ Vinylo-Individual/src/main/resources/db/migration/V1__CreateInitialTables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE artist
+(
+    id   int         NOT NULL AUTO_INCREMENT,
+    name varchar(50) NOT NULL,
+    bio  varchar(50) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (bio)
+);
+
+CREATE TABLE vinyl
+(
+    id          int     NOT NULL AUTO_INCREMENT,
+    vinylType   varchar(50) NOT NULL,
+    title       varchar(50) NOT NULL,
+    description varchar(50) NOT NULL,
+    isReleased  BOOLEAN NOT NULL,
+    artist_id   int,
+    PRIMARY KEY (id),
+    UNIQUE (description),
+    FOREIGN KEY (artist_id) REFERENCES artist (id)
+);
