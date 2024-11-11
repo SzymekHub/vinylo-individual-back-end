753733b9cd830f454e59c55ae97437bdd174e327
I540927 <s.gancarz@student.fontys.nl>
2024-11-11 11:01:39 +0100

    added @ActiveProfiles("test") to use the in memory database for testing

--- Vinylo-Individual/src/test/java/s3/individual/vinylo/controllers/VinylControllerTest.java
+++ Vinylo-Individual/src/test/java/s3/individual/vinylo/controllers/VinylControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import s3.individual.vinylo.domain.Artist;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // to
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 // That is needed to make sure all your security configuration is properly
 // loaded
