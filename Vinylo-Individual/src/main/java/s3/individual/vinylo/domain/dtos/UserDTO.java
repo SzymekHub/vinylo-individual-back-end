b79ba265bfbc4e36f9c49ff666a18ec8ebae5776
I540927 <s.gancarz@student.fontys.nl>
2024-11-19 12:13:38 +0100

    UserController Test

--- Vinylo-Individual/src/main/java/s3/individual/vinylo/domain/dtos/UserDTO.java
+++ Vinylo-Individual/src/main/java/s3/individual/vinylo/domain/dtos/UserDTO.java
@@ -1,5 +1,7 @@
 package s3.individual.vinylo.domain.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +10,17 @@ import lombok.Setter;
 public class UserDTO {
 
     private int id;
+
+    @NotBlank(message = "username must not be blank")
     private String username;
+
+    @NotBlank(message = "email must not be blank")
     private String email;
+
+    @NotBlank(message = "password must not be blank")
     private String password;
+
+    @NotNull(message = "Premium status is required")
     private Boolean isPremium;
 
 }
---
+++ Vinylo-Individual/src/test/java/s3/individual/vinylo/controllers/UserControllerTest.java
@@ -0,0 +1,195 @@
+package s3.individual.vinylo.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import s3.individual.vinylo.domain.User;
+import s3.individual.vinylo.services.UserService;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void testAddUser_ShouldCreateAndReturn201_WhenRequestValid() throws Exception {
+        // Arrange
+        User createdUser = User.builder()
+                .id(100)
+                .username("Username Test")
+                .email("UserTest@gmail.com")
+                .password("UserTestPass")
+                .isPremium(false)
+                .build();
+        // Act
+        when(userService.saveUser(eq(null), any(User.class))).thenReturn(createdUser);
+
+        // Assert
+        mockMvc.perform(post("/users")
+                .contentType(APPLICATION_JSON_VALUE)
+                .content("""
+                                    {
+                        "username": "Username Test",
+                        "email" : "UserTest@gmail.com",
+                        "password" : "UserTestPass",
+                        "isPremium" : true
+
+                                    }
+                                """))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().string("User created successfully"));
+
+        verify(userService).saveUser(eq(null), any(User.class));
+    }
+
+    @Test
+    void testAddUser_ShouldCreateAndReturn400_WhenMissingFields() throws Exception {
+        mockMvc.perform(post("/users")
+                .contentType(APPLICATION_JSON_VALUE)
+                .content("""
+                                    {
+                        "username": "Username Test",
+                        "email" : "",
+                        "password" : "UserTestPass",
+                        "isPremium" : true
+
+                                    }
+                                """))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(userService);
+    }
+
+    @Test
+    void testDeactivateUserById_shouldReturn201_WhenRequestValid() throws Exception {
+        // Arrange
+        int userId = 100;
+        when(userService.deativateUserById(userId)).thenReturn(true);
+
+        // Act and Assert
+        mockMvc.perform(delete("/users/{id}", userId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("User with id " + userId + " was successfully deleted."));
+
+        verify(userService).deativateUserById(userId);
+    }
+
+    @Test
+    void testDeativateUserById_ShouldReturn404_WhenUserNotFound() throws Exception {
+        // Arrange
+        int userId = 200;
+        when(userService.deativateUserById(userId)).thenReturn(false);
+
+        // Act and Assert
+        mockMvc.perform(delete("/users/{id}", userId))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("User with id " + userId + " was not found."));
+
+        verify(userService).deativateUserById(userId);
+    }
+
+    @Test
+    void testGetUsers_shouldReturn200RespondWithUsersArray() throws Exception {
+        // Arrange
+        List<User> users = List.of(
+                User.builder()
+                        .id(1)
+                        .username("UserName1")
+                        .email("Email1@gmail.com")
+                        .password("Password1")
+                        .isPremium(false)
+                        .build(),
+                User.builder()
+                        .id(2)
+                        .username("UserName2")
+                        .email("Email2@gmail.com")
+                        .password("Password2")
+                        .isPremium(true)
+                        .build());
+
+        // Act
+        when(userService.getUsers()).thenReturn(users);
+
+        // Assert
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.users[0].id").value(1))
+                .andExpect(jsonPath("$.users[0].username").value("UserName1"))
+                .andExpect(jsonPath("$.users[1].id").value(2))
+                .andExpect(jsonPath("$.users[1].username").value("UserName2"));
+
+        verify(userService).getUsers();
+    }
+
+    @Test
+    void testGetUser_shouldReturn200RespondWithUserByID() throws Exception {
+        // Arrange
+        int userId = 1;
+        User user = User.builder()
+                .id(userId)
+                .username("UserName1")
+                .email("Email1@gmail.com")
+                .password("Password1")
+                .isPremium(false)
+                .build();
+
+        when(userService.getUserById(userId)).thenReturn(user);
+
+        // Act and Assert
+        mockMvc.perform(get("/users/{id}", userId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.username").value("UserName1"))
+                .andExpect(jsonPath("$.email").value("Email1@gmail.com"))
+                .andExpect(jsonPath("$.isPremium").value(false));
+
+        verify(userService).getUserById(userId);
+    }
+
+    @Test
+    void testGetUser_shouldReturn404_WhenUserNotFound() throws Exception {
+        // Arrange
+        int userId = 999;
+        when(userService.getUserById(userId)).thenReturn(null);
+
+        // Act and Assert
+        mockMvc.perform(get("/users/{id}", userId))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("User record not found"));
+
+        verify(userService).getUserById(userId);
+    }
+}
